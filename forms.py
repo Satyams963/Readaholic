88309b8b1e12ad80f260a52bbc2c24a363d13b84
Satyam <Satyams963@gmail.com>
2023-04-18 19:07:35 +0530

    add session 12 and 13

--- forms.py
+++ forms.py
@@ -1,15 +1,15 @@
-from flask_wtf import Flaskform
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import Email, Length, EqualTo, DataRequired
 
-class AdminRegistrationForm(Flaskform):
+class AdminRegistrationForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired(), Length(min=6)])
     confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo('password')])
 
-    Submit = SubmitField("Register")
+    submit = SubmitField("Register")
 
-class AdminLoginForm(Flaskform):
+class AdminLoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired(), Length(min=6)])
-    Submit = SubmitField("Login")
+    submit = SubmitField("Login")
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+from forms import AdminRegistrationForm 
+from forms import AdminLoginForm
 
 app = Flask(__name__)
+
+app.config[ 'SECRET_KEY'] = 'secret_key_33'
 
 title = "Readaholic"
 data = {
@@ -16,9 +20,19 @@ def home():
 def about():
     return render_template("about.html")
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+    form = AdminRegistrationForm()
+    if form.validate_on_submit():
+        print(form.data)
+    return render_template("register.html", form = form)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = AdminLoginForm()
+    if form.validate_on_submit():
+        print(form.data)
+    return render_template("login.html", form = form)
 
 
 
